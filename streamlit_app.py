172a97c456850057fda33272444857da14fe5d98
871192i <65648092+871192i@users.noreply.github.com>
2024-10-25 17:02:49 +0000

    change2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My Streamlit app")
+st.title("🎈 My Streamlit clicks app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
